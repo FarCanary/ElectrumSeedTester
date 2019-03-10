c7010bc4f54f526f1c27d5e9a964ae508e7f5066
Kev <kev@Kevins-iMac.local>
2019-03-10 10:51:43 +0000

    Change Target name.

--- compile.py
+++ compile.py
@@ -43,7 +43,7 @@ for style in styles:
 
 # Write the standalone file
 
-f = open('bip39-standalone.html', 'w', encoding="utf-8")
+f = open('ElectrumSeedTester-standalone.html', 'w', encoding="utf-8")
 f.write(page)
 f.close()
 
